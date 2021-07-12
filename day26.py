f68f041c5cbb88cca339af8a7bc435303677a7fd
Jesuvi27 <85822757+Jesuvi27@users.noreply.github.com>
2021-07-12 10:15:45 +0530

    Create day26.py

---
+++ day26.py
@@ -0,0 +1,16 @@
+//•	Enumerate a python list and try to print the counter with the list value
+
+l1= ['Reena','Bala','Janu','Mano','Kani','Rohith']
+print(list(enumerate(l1)))
+
+OUTPUT:
+[(0, 'Reena'), (1, 'Bala'), (2, 'Janu'), (3, 'Mano'), (4, 'Kani'), (5, 'Rohith')]
+
+
+//•	Enumerate a python tuple and try to print the counter with the tuple value
+
+l1= ('Reena','Bala','Janu','Mano','Kani','Rohith')
+print(tuple(enumerate(l1)))
+
+OUTPUT:
+  ((0, 'Reena'), (1, 'Bala'), (2, 'Janu'), (3, 'Mano'), (4, 'Kani'), (5, 'Rohith'))
